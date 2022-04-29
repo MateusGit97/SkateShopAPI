a386416df3f15324377a9d0aefabd19ae6178c50
Mateus <mateusmarques51@gmail.com>
2022-04-29 17:17:13 -0300

    Add class Cliente

---
+++ SkateShopAPI/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkateShopAPI.Models
+{
+    public class Cliente
+    {
+        public int CPF { get; set; }
+        public string Nome { get; set; }
+        public int Telefone { get; set; }
+        public string Email { get; set; }
+    }
+}
