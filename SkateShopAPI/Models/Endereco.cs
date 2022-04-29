c28e9d106cd4db8f9c6998469979d774c6135dc3
Mateus <mateusmarques51@gmail.com>
2022-04-29 17:22:44 -0300

    Add class Endereco

---
+++ SkateShopAPI/Models/Endereco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkateShopAPI.Models
+{
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public int CEP { get; set; }
+        public string Complemento { get; set; }
+    }
+}
